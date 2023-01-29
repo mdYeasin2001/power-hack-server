97519f6d0a7761dd55c360d84865b3a8448c8165
Yeasin <yeasin30112001@gmail.com>
2023-01-29 23:31:12 +0600

    billing CRUD

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 const userRouter = require("./routes/userRoutes");
+const billRouter = require("./routes/billRoutes");
 
 const app = express();
 
@@ -17,6 +20,15 @@ app.use(
   })
 );
 
-app.use("/api/v1/users", userRouter);
+app.use("/api", userRouter);
+app.use("/api", billRouter);
+
+// 404
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// GLOBAL ERROR HANDLER
+app.use(globalErrorHandler);
 
 module.exports = app;
--- routes/billRoutes.js
+++ routes/billRoutes.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const billController = require("../controllers/billController");
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.post("/billing-list", billController.getBillList);
-router.post("/add-billing", billController.addBill);
-router.patch("/update-billing/:id", billController.updateBill);
-router.post("/delete-billing/:id", billController.deleteBill);
+router.get("/billing-list", authController.protect, billController.getBillList);
+router.post("/add-billing", authController.protect, billController.addBill);
+router.patch(
+  "/update-billing/:id",
+  authController.protect,
+  billController.updateBill
+);
+router.delete(
+  "/delete-billing/:id",
+  authController.protect,
+  billController.deleteBill
+);
 
 module.exports = router;
